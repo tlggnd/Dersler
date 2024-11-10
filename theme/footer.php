92bb3dd0141f82932201d726106a785bba316025
tlggnd <tolga.gundogdu91@gmail.com>
2024-11-10 22:26:35 +0300

    body arkaplan güncellendi

--- theme/footer.php
+++ theme/footer.php
@@ -1,7 +1,7 @@
 </body>
 
 <footer>
-    <p>&copy; 2024 Tolga Gündoğdu. Tüm hakları saklıdır.</p>
+    <p>&copy; 2024 Tolga Gündoğdu. Tüm hakları saklıdır. &#129488;</p>
 </footer>
 
 <script src=".\assets\js\app.js"></script>
